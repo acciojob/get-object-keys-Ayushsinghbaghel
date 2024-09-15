8296a533f36238e9cc8b22020e348a85928213d8
Ayush Singh <97668925+Ayushsinghbaghel@users.noreply.github.com>
2024-09-15 14:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+// Add getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Use the getKeys method on the student object
+console.log(student.getKeys());
